f1bdf893c4a44697f881bb1caf39cdad5947f4a1
Justfunc <99782305+justfunc@users.noreply.github.com>
2026-06-19 17:06:23 +0800

    add go

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -65,8 +65,8 @@ function git_sparse_clone() {
 # mv -f package/nginx feeds/packages/net/nginx
 # git_sparse_clone ariang https://github.com/laipeng668/packages net/ariang
 # mv -f package/ariang feeds/packages/net/ariang
-# git_sparse_clone master https://github.com/laipeng668/packages lang/golang
-# mv -f package/golang feeds/packages/lang/golang
+git_sparse_clone master https://github.com/laipeng668/packages lang/golang
+mv -f package/golang feeds/packages/lang/golang
 # git_sparse_clone frp-binary-toml https://github.com/laipeng668/packages net/frp
 # mv -f package/frp feeds/packages/net/frp
 # git_sparse_clone frp-toml https://github.com/laipeng668/luci applications/luci-app-frpc applications/luci-app-frps
